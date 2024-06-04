97b2088e82a1dde863e49042b7e4ccf2738210b8
jinchao <jinchao@meisyd.com>
2024-06-04 10:17:13 +0800

    fix: router

--- template/src/main.js
+++ template/src/main.js
@@ -2,11 +2,17 @@ import {
   createSSRApp
 } from "vue";
 import App from "./App.vue";
+{{#uni_simple_router}}
+import router from '@/router';
+{{/uni_simple_router}}
 
 export function createApp() {
   const app = createSSRApp(App);
 
   return {
     app,
+    {{#uni_simple_router}}
+    router,
+    {{/uni_simple_router}}
   };
 }
--- template/vite.config.js
+++ template/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
+{{#uni_simple_router}}
 import vitePluginUniRouter from './src/uni-simple-router/compiler';
+{{/uni_simple_router}}
 /**
  * @type {import('vite').UserConfig}
  */
 export default defineConfig({
   plugins: [
+    {{#uni_simple_router}}
     vitePluginUniRouter({
       pluginPath: './uni-simple-router',
       applet: {
@@ -16,6 +19,7 @@ export default defineConfig({
         },
       },
     }),
+    {{/uni_simple_router}}
     uni(),
   ],
 });
