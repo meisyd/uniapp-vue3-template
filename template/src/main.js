6d8022328e1a20b11510d393415c964131b01982
jinchao <jinchao@meisyd.com>
2024-06-04 13:32:47 +0800

    feat: lx-ui

--- template/src/main.js
+++ template/src/main.js
@@ -5,9 +5,12 @@ import App from "./App.vue";
 {{#uni_simple_router}}
 import router from '@/router';
 {{/uni_simple_router}}
+import lxUI from 'lx-ui';
 
 export function createApp() {
   const app = createSSRApp(App);
+
+  app.use(lxUI);
 
   return {
     app,
